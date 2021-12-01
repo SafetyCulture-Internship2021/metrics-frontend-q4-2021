5df7f251b65770d3c5db3559a1c8068481332812
Brody Jorgensen <jorgo9110@gmail.com>
2021-12-01 11:14:52 +1000

    Attempt connection with back end through database hoever unsuccessful.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import axios from "axios";
 import { useAuth } from "../hooks/auth";
 import "../App.css";
 
+
 export const Home = () => {
     const { authenticatedRequest } = useAuth();
 
     const [isLoading, setIsLoading] = useState(true);
     const [account, setAccount] = useState(null);
+   /* const [newData, setNewData] = useState(null);*/
+    /*const [ping, setPingData] = useState(null);*/
 
 
     useEffect(() => {
@@ -16,7 +19,18 @@ export const Home = () => {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
-            });
+                 });/*
+                 await axios.get('/services').then(res =>{
+                     const newData = JSON.stringify(res.data);
+                     setNewData(newData)
+                     return newData;
+                 });*/
+                /*await axios.get('/metrics/input').then(res => {
+                    console.log(res);
+                    const ping = res.data;
+                    setPingData(ping)
+                    return ping;
+                })*/
 
 
 
@@ -29,9 +43,14 @@ export const Home = () => {
         return <div>Loading...</div>;
     }
 
-    return (<>Welcome to the app {account.account_name}
+    return (<>Welcome to the app {account.account_name}.
     <div>
         <br/> This is the metrics front end that displays information
+        {/*<br/> name: {ping.pod_id}
+        <br/> time stamp: {ping.time_stamp}
+        <br/>http status code: {JSON.stringify(ping.http_status)}
+        <br/> avg latency: {ping.avg_latency}
+        <br/>99th percentile: {ping.percentile_99}*/}
 
     </div>
     </>);
