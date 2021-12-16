13a7044598e2736dcd97bcaf91d0128fd4fdfb90
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2021-12-17 08:50:52 +1000

    Attempted to update Dropdown page with handler function

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,14 @@
 import { useState } from 'react';   
+import Graph from "./Graph"; 
+/*import React from "react";
+import { useTheme } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { withRouter } from "react-router-dom";*/
 
 function Dropdown( {selected, setSelected}) {
     const [isActive, setIsActive] = useState(false);
-    const options = ['Authorization', 'Users', 'Cart', 'Suggestions', 'Billing']
+    const options = [   'Authorization', 'Users', 'Cart', 'Suggestions', 'Billing']
 
     return (
         <div className='dropdown'>
@@ -28,3 +34,76 @@ function Dropdown( {selected, setSelected}) {
 }
 
 export default Dropdown;
+
+/*const Header = props => {
+    const { history } = props;
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const theme = useTheme();
+  
+    const handleMenu = event => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleMenuClick = pageURL => {
+      history.push(pageURL);
+      setAnchorEl(null);
+    };
+
+    const menuItems = [
+        {
+          menuTitle: "Authorization",
+          pageURL: "/"
+        },
+        {
+          menuTitle: "User",
+          pageURL: "/"
+        },
+        {
+          menuTitle: "Cart",
+          pageURL: "/graph"
+        }
+      ];
+
+      return (
+        <div className={classes.root}>
+            <Dropdown
+                edge="star t"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </Dropdown>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                {menuItems.map(menuItem => {
+                  const { menuTitle, pageURL } = menuItem;
+                  return (
+                    <MenuItem onClick={() => handleMenuClick(pageURL)}>
+                      {menuTitle}
+                    </MenuItem>
+                  );
+                })}
+              </Menu>
+            )
+        </div>
+    );
+};
+
+export default withRouter(Header);*/
